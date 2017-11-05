0aab08750622eec1d19d0bb7ffc333895a901493
Conner Lane <cal0043@auburn.edu>
2017-11-05 01:22:51 -0500

    rename stuff

--- background.py
+++
@@ -1,4 +0,0 @@
-from appscript import app, mactypes
-
-	def changeBackgroud(bg)
-		app('Finder').desktop_picture.set(mactypes.File('bg'))
--- text.py
+++ text.py
@@ -1,17 +1,17 @@
 import boto3
 
-# Create an SNS client
 client = boto3.client(
     "sns",
-    aws_access_key_id="AKIAJWF5KX6Q22U6WJCQ",
-    aws_secret_access_key="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",
+    aws_access_key_id="AKIAJOD3QFUXOUAZOIDA",
+    aws_secret_access_key="nCBDa0W2iNHeyZoYMH8ORII3c1WAtt5LTYFAYvoR",
     region_name="us-east-1"
 )
 
-# Send your sms message.
-client.publish(
-    PhoneNumber="+12512293283",
-    Message ="Fruit salad, yummy yummy."
-)
+if __name__ == "__main__":
+
+    client.publish(
+        PhoneNumber="+12512293283",
+        Message ="Fruit saslad, yummy yummy."
+    )
 
 
